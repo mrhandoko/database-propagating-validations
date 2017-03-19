943be1d428690e040cf7a7e9ef6599268fc5adc7
Bambang Handoko <bambank.handoko@yahoo.com>
2017-03-19 18:10:05 +0700

    adding schema into models

---
+++ models/users.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const validate = require('mongoose-validator')
+
+let email_validation = [
+  validate({
+    validator: 'isEmail',
+    message: 'Email is nof valid!'
+  })
+]
+
+let date_validation = [
+  validate({
+    validator: 'isDate',
+    message: 'Format date is not valid! example :yyyy/mm/dd'
+  })
+]
+
+
+let organizerSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, 'Title is required. Please fill this form'],
+    unique: [true, 'Title is already in used']
+  },
+  username: {
+    type: String,
+    required: [true, 'Username is required. Please fill this form']
+  },
+  data: {
+    type: Date,
+    required: [true, 'Date is required. Please fill this form']
+    validate: date_validation
+  }
+})
+
+let Organizer = mongoose.model('organizer', organizerSchema)
+
+module.exports = Organizer
